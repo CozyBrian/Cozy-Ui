dcd37580357d4027bbc3e892b7fc73af3b864cc2
CozyBrian <dragonbrian33@gmail.com>
2023-01-23 22:46:58 +0000

    add: framer motion image

--- src/components/Img/Img.tsx
+++ src/components/Img/Img.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 export interface ImgProps {
   src: string;
   alt: string;
   className?: string;
   style?: React.CSSProperties;
+  reff?: React.RefObject<HTMLImageElement>;
 }
 
-const Img = ({src, alt, className, style} : ImgProps) => {
-  return <img style={style} className={className} src={src} alt={alt} />;
+const Img = ({src, alt, className, style, reff} : ImgProps) => {
+  const [loaded, setLoaded] = useState(false);
+  return (
+    <motion.img
+      ref={reff}
+      style={style}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: loaded ? 1 : 0 }}
+      exit={{ opacity: 0 }}
+      onLoad={() => setLoaded(true)}
+      className={className}
+      src={src}
+      alt={alt}
+    />
+  );
 };
 
 export default Img;
